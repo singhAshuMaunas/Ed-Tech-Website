cad7ef0cc395f6cce5308cc23cd3d5a15d45bd2e
Ashutosh Singh <102754668+singhAshuMaunas@users.noreply.github.com>
2023-07-17 20:24:08 +0530

    Update Profile.js

--- server/routes/Profile.js
+++ server/routes/Profile.js
@@ -7,6 +7,7 @@ const {
   getAllUserDetails,
   updateDisplayPicture,
   getEnrolledCourses,
+  instructorDashboard,
 } = require("../controllers/Profile")
 
 // ********************************************************************************************************
@@ -19,5 +20,5 @@ router.get("/getUserDetails", auth, getAllUserDetails)
 // Get Enrolled Courses
 router.get("/getEnrolledCourses", auth, getEnrolledCourses)
 router.put("/updateDisplayPicture", auth, updateDisplayPicture)
-
+router.get("/instructorDashboard", auth, isInstructor, instructorDashboard)
 module.exports = router
